609eaaccbde3c508fbf720795a324f7352d3ff8f
Carlos Rojas <guarrodesign@gmail.com>
2023-02-02 09:52:24 -0500

    added original src folder with first code version

---
+++ src/pages/para-empresas/index.js
@@ -0,0 +1,79 @@
+import * as React from "react"
+import './para-empresas.scss'
+
+
+
+
+const IndexPage = () => {
+  return (
+    <main>
+
+      <header className="siteHeader">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </header>
+
+
+
+
+      <section>
+        <div className="boxed">
+          <h1 className="heading_h1">Headline 1. Lorem ipsum dolor sit amet, consectetur</h1>
+        </div>
+      </section>
+
+
+
+
+
+
+      <h2 className="heading_h2">Titulo H2</h2>
+      <h3 className="heading_h3">tITULO h3</h3>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <footer className="siteFooter">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </footer>
+
+
+
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
